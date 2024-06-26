7b377932ebae9723451b6261ed830e1fea5f5053
itsJeetJayas7 <jjayas@purdue.edu>
2024-06-25 20:44:11 -0400

    Included User Authentication Pages

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
+
+
 def register(request):
     if request.method == 'POST' :
         form = UserRegisterForm(request.POST)
@@ -8,7 +11,11 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
