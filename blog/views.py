03a33d8a673e6c8f707114b05fa09f95f92724f4
itsJeetJayas7 <jjayas@purdue.edu>
2024-07-04 12:52:00 -0400

    Pagination and Posts by specific user added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from  .models import Post
 
@@ -14,7 +15,21 @@ class PostListView(ListView):
     model = Post
     template_name = "blog/home.html"
     context_object_name = 'posts'
-    ordering = ["-date_posted"]
+    ordering = ['-date_posted']
+    paginate_by = 7
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = "blog/user_posts.html"
+    context_object_name = 'posts'
+    paginate_by = 11
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by("-date_posted")
+
+
+
 
 class PostDetailView(DetailView):
     model = Post
